8b14b6c50f0bfb55bd40098ac5d333aa67b0a727
WhyK <windchimeyk@gmail.com>
2024-08-16 21:32:58 +0900

    style: biome check

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,4 +3,5 @@ import { pingCommand } from "./ping";
 const commands = [pingCommand];
 export const commandsConfig = commands.map((item) => item.config);
 export const commandsName = commands.map((item) => item.config.name);
-export const getCommandByName = (name: string) => commands.find((item) => item.config.name === name)
+export const getCommandByName = (name: string) =>
+  commands.find((item) => item.config.name === name);
